c2fea8d84dda476e6def7aad4919049133eb3634
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2020-12-28 19:11:59 +0600

    Done input basic

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+      Home Page
+      <br>
+      <Form></Form>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Form from "./Components/Form/Form";
+import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default {
-  name: 'App',
+  data(){
+    return {
+
+    }
+  },
   components: {
-    HelloWorld
+    Form
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
